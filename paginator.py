5a02bf945712a897f7ef09401f1915844c4866af
kashishsahu <kashishsahu257@gmail.com>
2021-12-01 14:42:53 +0530

    paginating concept

---
+++ paginator.py
@@ -0,0 +1,14 @@
+import boto3
+
+client = boto3.client('iam')
+
+paginator =client.get_paginator('list_roles')
+
+#print(paginator)
+
+for page in paginator.paginate():
+    for r in page['Roles']:
+        print("Role name :",r['RoleName'])
+     
+
+
