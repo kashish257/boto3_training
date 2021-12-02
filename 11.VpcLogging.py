71e8b736c894a38a97ec69f2eff47614ba7f6e55
kashishsahu <kashishsahu257@gmail.com>
2021-12-02 15:55:01 +0530

    hk

--- 11.VpcLogging.py
+++ 11.VpcLogging.py
@@ -2,15 +2,17 @@ import boto3
 import logging
 
 logger =logging.getLogger()
-logger.basicConfig(level=logging.INFO,format='%(asctime)s:%(levelname)s:%(messages)s')
+#logger.basicConfig(level=logging.INFO,format='%(asctime)s:%(levelname)s:%(messages)s')
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
-vpcclient=boto3.client('ec2')
+vpcclient=boto3.client('ec2',region_name="us-east-2")
 
 def fnvpcdelete(vpcId):
     from botocore.exceptions import ClientError 
     vpc=None
     try:
-        vpc=vpcclient.delete_vpc(Vpc_id=vpcId)
+        vpc=vpcclient.delete_vpc(VpcId=vpcId)
         logger.info("listing vpc")
 
     except ClientError as ce:
@@ -20,6 +22,6 @@ def fnvpcdelete(vpcId):
 
 if __name__=="__main__":
     
-    vpc=fnvpcdelete(vpcId="vpc-019568ee2d832946d")
+    vpc=fnvpcdelete(vpcId="vpc-0ec3d57538331d1d1")
     print("vpc")
 
